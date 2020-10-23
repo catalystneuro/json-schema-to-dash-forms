d40d43a56d07ffed4e049b57d3b91598eb0c704b
!git for-each-ref --format='%(refname:short)' `git symbolic-ref HEAD` <ben.dichter@gmail.com>
2020-10-23 11:50:58 -0400

    style suggestions

--- json_schema_to_dash_forms/utils.py
+++ json_schema_to_dash_forms/utils.py
@@ -1,12 +1,12 @@
-import dash_core_components as dcc
-import dash_html_components as html
-import dash_bootstrap_components as dbc
-from dash.dependencies import Input, Output, State
-from datetime import datetime
-from dash_cool_components import KeyedFileBrowser
 import os
-from pathlib import Path
+from datetime import datetime
+
 import dash
+import dash_bootstrap_components as dbc
+import dash_html_components as html
+from dash.dependencies import Input, Output, State
+from dash_cool_components import KeyedFileBrowser
+from pathlib import Path
 
 
 def make_filebrowser_modal(parent_app, modal_id="modal-filebrowser", display=None):
