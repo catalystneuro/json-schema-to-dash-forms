36c19ed1dc9bff2de30e30c848d5b1bfc368330f
luiz <luiz_tauffer@hotmail.com>
2021-08-09 14:29:15 +0200

    update version

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open('requirements.txt') as f:
 
 setup(
     name='json_schema_to_dash_forms',
-    version='0.1.7',
+    version='0.1.8',
     description='JSON schema to Dash forms',
     long_description=long_description,
     long_description_content_type='text/markdown',
